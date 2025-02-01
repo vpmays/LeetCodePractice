716635a63571dc330ee8e08fc098ca8f4c25d639
Vincent Mays <vpmays@mail.fhsu.edu>
2025-02-01 00:45:49 -0700

    added in new challenges

--- binarySearch.py
+++ binarySearch.py
@@ -1,3 +1,35 @@
+def vincentBinarySearch(arr, target, low, high):
+    while(high >= low):
+        mid = int((high - low)/2) + low
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
+myArr = [1,3,4,7,8,10,12,14,23,24,32,44]
+target1 = 32
+print(vincentBinarySearch(myArr, target1, 0, len(myArr)-1))
+
+def vincentRecursiveBinarySearch(arr, target, low, high):
+    if high >= low:
+        mid = int((high - low)/2) + low
+        if arr[mid] == target:
+            return mid
+        elif target > arr[mid]:
+            return vincentRecursiveBinarySearch(arr, target, mid + 1, high)
+        else:
+            return vincentRecursiveBinarySearch(arr, target, low, mid - 1)
+    else:
+        return -1
+
+print(vincentRecursiveBinarySearch(myArr, target1, 0, len(myArr)-1))
+
+
+################################################################################
 '''
 # Looped Binary Search in python
 def binarySearch(array, x, low, high):
@@ -63,33 +95,3 @@ if result != -1:
 else:
     print("Not found")
 '''
-
-def vincentBinarySearch(arr, target, low, high):
-    while(high >= low):
-        mid = int((high - low)/2) + low
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] > target:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return -1
-
-
-myArr = [1,3,4,7,8,10,12,14,23,24,32,44]
-target1 = 32
-print(vincentBinarySearch(myArr, target1, 0, len(myArr)-1))
-
-def vincentRecursiveBinarySearch(arr, target, low, high):
-    if high >= low:
-        mid = int((high - low)/2) + low
-        if arr[mid] == target:
-            return mid
-        elif target > arr[mid]:
-            return vincentRecursiveBinarySearch(arr, target, mid + 1, high)
-        else:
-            return vincentRecursiveBinarySearch(arr, target, low, mid - 1)
-    else:
-        return -1
-
-print(vincentRecursiveBinarySearch(myArr, target1, 0, len(myArr)-1))
--- numberOfIslands.py
+++ numberOfIslands.py
@@ -1,16 +1,50 @@
 def numIslands(grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        
-        return
+  """
+  :type grid: List[List[str]]
+  :rtype: int
+  """
+  lands = []
+  dirs = [[0,1],[0,-1],[1,0],[-1,0]]
+  islands = []
 
-grid = [
+  for rowIndex in range(0, len(grid)):
+    for columnIndex in range(0, len(grid[0])):
+      queue = []
+      #check if node is "1", if not, do nothing
+      if grid[rowIndex][columnIndex] == "1":
+        if [rowIndex,columnIndex] not in lands:
+          islands.append([rowIndex,columnIndex])
+          queue.append([rowIndex,columnIndex])
+      #breadth first search to find all neighbors
+      while queue:
+        currentNode = queue.pop(0)
+        currentRowIndex = currentNode[0]
+        currentColumnIndex = currentNode[1]
+        #loop through right,left,up,down and see if land is adjacent, if so add the node to lands
+        for dir in dirs:
+          currentRowIndex = currentRowIndex + dir[0]
+          currentColumnIndex = currentColumnIndex + dir[1]
+          #make sure you're in bounds
+          if (0<=currentRowIndex<len(grid)) and (0<=currentColumnIndex<len(grid[0])):
+            #see if neighbor node is land, add it to lands if so
+            if grid[currentRowIndex][currentColumnIndex] == "1":
+              #make sure node is not already in lands
+              if [currentRowIndex,currentColumnIndex] not in lands:
+                lands.append([currentRowIndex,currentColumnIndex])
+                queue.append([currentRowIndex,currentColumnIndex]) #come check this later
+          #after checking a direction, return back to current node
+          currentRowIndex = currentNode[0]
+          currentColumnIndex = currentNode[1]
+
+
+  return len(islands)
+  
+
+grid1 = [
   ["1","1","1","1","0"],
   ["1","1","0","1","0"],
   ["1","1","0","0","0"],
   ["0","0","0","0","0"]
 ]
 
-numIslands(grid)
+print(numIslands(grid1))
